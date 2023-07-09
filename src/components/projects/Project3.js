48abbb9d355052652596c19a4b13d4d0b4699a33
Davyd Sadovskyy <sadovsd@miamioh.edu>
2023-07-09 19:53:06 -0400

    add favicon.ico and meta description

--- src/components/projects/Project3.js
+++ src/components/projects/Project3.js
@@ -8,6 +8,9 @@ class Project3 extends Component {
             <div className='container mt-20'>
                 <h1>Project 3</h1>
                 <h3 className='mt-8'>coming soon...</h3>
+                <div className='border'>
+                    <h3>DS</h3>
+                </div>
             </div>
         );
     }
